8bcf8c98dc747d520bec0df6b759cb87b0a68f19
Suzane Duarte <ssuzane9@hotmail.com>
2022-04-25 19:16:47 -0300

    Limit player movement

--- src/events.c
+++ src/events.c
@@ -7,4 +7,9 @@ void handle_events(bool keys[], player *p1, player *p2){
 	if(keys[DOWN]) p2->player.y += PLAYER_SPEED;
 	if(keys[LETTER_W]) p1->player.y -= PLAYER_SPEED;
 	if(keys[LETTER_S]) p1->player.y += PLAYER_SPEED;
+
+	if(p2->player.y > 440) p2->player.y = 440;
+	if(p2->player.y < 70) p2->player.y = 70;
+	if(p1->player.y > 440) p1->player.y = 440;
+	if(p1->player.y < 70) p1->player.y = 70;
 }
